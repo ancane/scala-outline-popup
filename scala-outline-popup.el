f45b32dbfe192f5f69dbe7e34fc6e374460d3bc5
Igor Shymko <i.shymko@thenewmotion.com>
2014-11-14 11:02:58 +0200

    popup version fix

--- scala-outline-popup.el
+++ scala-outline-popup.el
@@ -4,7 +4,7 @@
 
 ;; Author: Igor Shymko <igor.shimko@gmail.com>
 ;; Version: 0.1
-;; Package-Requires: ((dash "2.9.0") (popup "20141106.455") (scala-mode2 "0.22"))
+;; Package-Requires: ((dash "2.9.0") (popup "20141002.320") (scala-mode2 "0.22"))
 ;; Keywords: scala, structure, summary
 ;; URL: https://github.com/ancane/scala-outline-popup.el
 
